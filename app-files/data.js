91b4ae043abe63e071514b9fee2884339b229546
panorama-360 <tebfor11@gmail.com>
2025-08-30 12:03:29 +0700

    Commit

---
+++ app-files/data.js
@@ -0,0 +1,114 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20250719_133236_00_0121",
+      "name": "IMG_20250719_133236_00_012(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.024113130505707403,
+          "pitch": 0.0029448332502397534,
+          "rotation": 0,
+          "target": "0-img_20250719_133236_00_0121"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20250719_114121_00_008",
+      "name": "IMG_20250719_114121_00_008",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.014727261515195877,
+          "pitch": 0.2999314389505443,
+          "rotation": 3.141592653589793,
+          "target": "1-img_20250719_114121_00_008"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
